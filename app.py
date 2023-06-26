8176c81ef084a8ea3a5b045a03447f7bb1c3613f
Saarthak Mudigere <saarthakmudigere@gmail.com>
2023-06-26 00:01:54 -0500

    2nd commit done!

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ cursor = conn.cursor()
 # Redis connection details
 redis_host = 'Redis-1002119262-SaarthakMudigereGirish.redis.cache.windows.net'
 redis_port = 6379
-redis_password = 'My2SymFLIM78MdHnE61sCcWkM6G0HGWd6AzCaKwIZUU='
+redis_password = 'l2CPHbqqk4w90H19Tmvm0n2ykWFLefcyBAzCaA1MX8M='
 
 # Redis client
 redis_client = redis.Redis(host=redis_host, port=redis_port, password=redis_password)
@@ -116,6 +116,38 @@ def page3():
 
     print(query_time)
     return render_template("3)Page.html", query_time=query_time, time_query=time_query, redis_time=redis_time)
+
+
+@app.route("/page4/", methods=['GET', 'POST'])
+def page4():
+    time_query = []
+    query_time = []
+    redis_time = []
+    if request.method == "POST":
+        lat = request.form['lat']
+        long = request.form['long']
+        for i in range(30):
+            time_query.append(i + 1)
+
+        query = "SELECT place FROM dbo.all_month TABLESAMPLE(1000 ROWS) WHERE (6371 * ACOS(COS(RADIANS(?)) * COS(RADIANS(latitude)) * COS(RADIANS(longitude) - RADIANS(?)) + SIN(RADIANS(?)) * SIN(RADIANS(latitude)))) <= 100; "
+        for i in time_query:
+            start = time.time()
+            cursor.execute(query, lat, long, lat)
+            end = time.time()
+            query_time.append(end-start)
+
+            rows = cursor.fetchall()
+            temp_result = ""
+            for j in rows:
+                temp_result = temp_result + str(j)
+
+            redis_client.set(i, temp_result)
+            s = time.time()
+            temp = redis_client.get(i)
+            e = time.time()
+            redis_time.append(e - s)
+
+    return render_template("4)Page.html", query_time=query_time, time_query=time_query, redis_time=redis_time)
 
 
 if __name__ == "__main__":
